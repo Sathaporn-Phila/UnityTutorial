c167a1f26f7f8c7355ed72ebae6e6bafb2b34c34
Sathaporn-Phila <nuinee_2544@hotmail.com>
2021-12-03 15:32:16 +0700

    set state for character idle or walking

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,14 +5,19 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     // Start is called before the first frame update
+    Vector3 m_Movement;
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        m_Movement.Set(horizontal,0f,vertical);
+        m_Movement.Normalize();
         
     }
 }
