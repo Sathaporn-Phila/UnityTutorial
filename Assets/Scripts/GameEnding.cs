499c9f711a92afa5f96f9bd0d215c7e712f745b0
Sathaporn-Phila <nuinee_2544@hotmail.com>
2021-12-06 13:49:24 +0700

    exit image can fade in

--- Assets/Scripts/GameEnding.cs
+++ Assets/Scripts/GameEnding.cs
@@ -3,11 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameEnding : MonoBehaviour {
-
-    private void Start() {
+    public float fadeDuration = 1f;
+    public float displayImageDuration = 1f;
+    public GameObject player;
+    public CanvasGroup exitBackgroundImageGroup;
+    public bool m_IsPlayerAtExit;
+    float m_timer;
+    void Start() {
         
     }
-    private void Update() {
-        
+    void Update() {
+        if(m_IsPlayerAtExit){
+            this.EndLevel();
+        }
+    }
+    void OnTriggerEnter(Collider other) {
+        if(other.gameObject == player){
+            m_IsPlayerAtExit = true;
+        }
+    }
+    void EndLevel(){
+        m_timer += Time.deltaTime;
+        exitBackgroundImageGroup.alpha = m_timer/fadeDuration;
+        if(m_timer>fadeDuration + displayImageDuration){
+            Application.Quit();
+        }
     }
 }
