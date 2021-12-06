60bcb9386969348ca0d340c92c2b8aa97e87a2e7
Sathaporn-Phila <nuinee_2544@hotmail.com>
2021-12-06 12:32:34 +0700

    add game ending script

---
+++ Assets/Scripts/GameEnding.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEnding : MonoBehaviour {
+
+    private void Start() {
+        
+    }
+    private void Update() {
+        
+    }
+}
