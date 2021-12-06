18dedfce293935b6edca9c175273e1b6eb3594e5
Sathaporn-Phila <nuinee_2544@hotmail.com>
2021-12-06 15:32:52 +0700

    add line of sight to check what it is hit.

---
+++ Assets/Scripts/GargoyleObserver.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GargoyleObserver : MonoBehaviour {
+    public Transform player;
+    public bool m_IsPlayerInRange;
+    public GameEnding gameEnding;
+
+    void Start(){
+
+    }
+    void Update(){
+        if(m_IsPlayerInRange){
+            Vector3 direction = player.position - transform.position + Vector3.up; //vector between gargoyle and john lemon
+            Ray ray = new Ray (transform.position,direction); // line of sight to that vector direction
+            RaycastHit raycastHit; //if hit something
+            if(Physics.Raycast(ray,out raycastHit)){
+                if(raycastHit.collider.transform == player){// line of sight hit the player,not the wall or floor; 
+
+                }
+            }
+        }
+    }
+    void OnTriggerEnter(Collider other) {
+        if(other.transform == player){
+            m_IsPlayerInRange = true;
+        }
+    }
+    void OnTriggerExit(Collider other) {
+        if(other.transform == player){
+            m_IsPlayerInRange = false;
+        }
+    }
+}
