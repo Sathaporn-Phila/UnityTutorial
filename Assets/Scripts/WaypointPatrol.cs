79462a2a7d3c444fcdabdfa2bfcda9d544f212c9
Sathaporn-Phila <nuinee_2544@hotmail.com>
2021-12-08 12:08:19 +0700

    Add AI to ghost

---
+++ Assets/Scripts/WaypointPatrol.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class WaypointPatrol : MonoBehaviour
+{
+    public NavMeshAgent navMeshAgent;
+    public Transform[] waypoints;
+    int m_CurrentWaypointsIndex;
+    void Start()
+    {
+        navMeshAgent.SetDestination(waypoints[0].position);
+    }
+
+    
+    void Update()
+    {
+        if(navMeshAgent.remainingDistance < navMeshAgent.stoppingDistance){
+            m_CurrentWaypointsIndex = (m_CurrentWaypointsIndex + 1)% waypoints.Length;
+            navMeshAgent.SetDestination(waypoints[m_CurrentWaypointsIndex].position);
+        }
+    }
+}
